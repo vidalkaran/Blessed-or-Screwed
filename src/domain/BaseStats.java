2ec0eb016c218061927392315f20e0d94f635b77
Vidal Karan <vidal.karan11@stjohns.edu>
2016-07-25 18:33:15 -0400

    Started Domain Branch
    -Began work on Unit.
    -Changed validSpecials and promotions to arrays.
    -added arrays for growths and max mods in character.
    -added code to retrieve specific stats from BaseStats
    -changed java version to 1.7

--- src/domain/BaseStats.java
+++ src/domain/BaseStats.java
@@ -19,6 +19,27 @@ public class BaseStats {
 		return revelations;
 	}
 	
+	public int getStats(String route, int stat)
+	{
+		int output = 0;
+		switch(route)
+		{
+			case "conquest":
+			{
+				output = conquest[stat];
+			}
+			case "birthright":
+			{
+				output = birthright[stat];
+			}
+			case "revelations":
+			{
+				output = revelations[stat];
+			}
+		}
+		return output;
+	}
+	
 	public String toString() {
 		return "Conquest bases: " + Arrays.toString(conquest) + ", Birthright bases: " + Arrays.toString(birthright) + ", Revelations bases: " + Arrays.toString(revelations);
 	}
