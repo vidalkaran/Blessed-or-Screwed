6d55bbf675af9cbf48e0e7e372aa1d1b51ae3061
Vidal Karan <vidal.karan11@stjohns.edu>
2016-07-26 12:08:09 -0400

    Finishing Unit
    -Changed function calculateStats() to calculateAverageStats()
    -moved the level difference part of the equation in
    calculateAverageStats to outside of the loop.
    -fixed issue with equation (Growths should be a decimal, not a whole
    number, Silas with 139 Strength :O))
    -added all methods to the constructor.
    -added printUnit method to Unit
    -Added new testing class for testing Unit, and tested Unit.

--- src/domain/Unit.java
+++ src/domain/Unit.java
@@ -7,22 +7,30 @@ public class Unit {
 	private Character myCharacter;
 	private Job myJob;
 	private int level;
-	private int stat[];
-	private int growths[];
-	private int maxstats[];
+	private double avgStat[] = new double[8];
+	private int growths[] = new int[8];
+	private int maxstats[] = new int[8];
 	private String route;
 	
-	public Unit(Character inputCharacter, Job inputJob, int Level)
+	public Unit(Character inputCharacter, Job inputJob, int inputLevel, String inputRoute)
 	{
 		myCharacter = inputCharacter;
 		myJob = inputJob;
+		level = inputLevel;
+		route = inputRoute;
+		
+		calculateGrowths();
+		calculateMaxStats();
+		calculateAverageStats(level, route);
 	}
 	
-	private void calculateStats(int inputLevel, String route) // gonna need to track route also
+	private void calculateAverageStats(int inputLevel, String route) // calculates average stats
 	{
+		int level = inputLevel - (myCharacter.getBaseStats().getStats(route, 0));//gets the level difference.
+		
 		for (int i = 0; i<=7; i++)
 		{
-			stat[i] = ((inputLevel - (myCharacter.getBaseStats().getStats(route, 0))) * growths[i]) + myCharacter.getBaseStats().getStats(route,i+1);
+			avgStat[i] = (level * (growths[i]/100)) + myCharacter.getBaseStats().getStats(route,i+1); //(level difference * growth + base) 
 		}
 	}
 	
@@ -42,5 +50,27 @@ public class Unit {
 		}
 	}
 	
+	public void printUnit()
+	{
+		System.out.println("Name: "+myCharacter.getName()
+							+"\n"+"Class: "+myJob.getName()
+							+"\n"+"Level: "+level
+							+"\n" + "Route: " + route);
+		for(int i = 0; i<=7; i++)
+		{
+			System.out.println("GROWTHS...");
+			System.out.println(growths[i]);
+		}
+		for(int i = 0; i<=7; i++)
+		{
+			System.out.println("MAX STATS...");
+			System.out.println(maxstats[i]);
+		}
+		for(int i = 0; i<=7; i++)
+		{
+			System.out.println("STATS...");
+			System.out.println(avgStat[i]);
+		}
+	}
 	
 }
