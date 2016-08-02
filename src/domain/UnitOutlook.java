051e93dabf55cb6d01d02cf1c0749ee49072be44
Vidal Karan <vidal.karan11@stjohns.edu>
2016-08-02 13:12:52 -0400

    Added rough UI, starting UnitOutlook
    Added rough UI, starting UnitOutlook

---
+++ src/domain/UnitOutlook.java
@@ -0,0 +1,15 @@
+package domain;
+
+//This class in an character arraylist holding multiple instances of one character, throughout their game lifespan.
+public class UnitOutlook {
+
+	Unit Unit;
+	
+	public UnitOutlook(Unit inputUnit, int level)
+	{
+		
+	}
+	
+//	Calculate
+	
+}
